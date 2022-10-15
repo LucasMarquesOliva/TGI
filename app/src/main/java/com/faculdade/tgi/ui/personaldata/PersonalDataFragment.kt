728d57ef6438331d43a1c03f5adf0a35f2915f3c
LucasMarquesOliva <marquesoliva.lucas@gmail.com>
2022-10-15 00:26:11 -0300

    Atualização da tela de dados pessoais (Exibição)

--- app/src/main/java/com/faculdade/tgi/ui/personaldata/PersonalDataFragment.kt
+++ app/src/main/java/com/faculdade/tgi/ui/personaldata/PersonalDataFragment.kt
@@ -1,14 +1,18 @@
 package com.faculdade.tgi.ui.personaldata
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.faculdade.tgi.R
 import com.faculdade.tgi.databinding.FragmentPersonalDataBinding
+import com.faculdade.tgi.ui.personaldataform.PersonalDataFormActivity
 
-class PersonalDataFragment : Fragment() {
+class PersonalDataFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentPersonalDataBinding? = null
     private val binding get() = _binding!!
@@ -19,18 +23,54 @@ class PersonalDataFragment : Fragment() {
         viewModel = ViewModelProvider(this)[PersonalDataViewModel::class.java]
         _binding = FragmentPersonalDataBinding.inflate(inflater, container, false)
 
+        binding.buttonEdit.setOnClickListener(this)
+
+        viewModel.loadData()
         observe()
 
         return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.loadData()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.button_edit -> startActivity(Intent(context, PersonalDataFormActivity::class.java))
+        }
+    }
+
     private fun observe() {
-        viewModel.text.observe(viewLifecycleOwner) {
-            binding.textPersonalData.text = it
+        viewModel.loadData.observe(viewLifecycleOwner) {
+            binding.textName.text = it.name
+            binding.textAge.text = it.age.toString()
+            binding.textWeight.text = it.weight.toString()
+            binding.textHeight.text = it.height.toString()
+            binding.textBmi.text = it.bmi.toString()
+            binding.textGlucose.text = it.glucose.toString()
+            binding.textInsulin.text = it.insulin.toString()
+            binding.textBloodPressure.text = it.bloodPressure.toString()
+            binding.textSkinThickness.text = it.skinThickness.toString()
+            binding.textDiabetesPedigree.text = it.diabetesPedigree.toString()
+            binding.textPregnancies.text = it.pregnancies.toString()
+
+            //Exibe a quantidade de vezes que engravidou apenas se for do sexo feminino
+            if (it.gender.toString().toInt() == 0) {
+                binding.textGender.text = resources.getString(R.string.male)
+                binding.viewPregnancies.isVisible = false
+                binding.linearPregnancies.isVisible = false
+            } else {
+                binding.textGender.text = resources.getString(R.string.female)
+                binding.viewPregnancies.isVisible = true
+                binding.linearPregnancies.isVisible = true
+            }
         }
     }
 }
--- app/src/main/java/com/faculdade/tgi/ui/personaldata/PersonalDataViewModel.kt
+++ app/src/main/java/com/faculdade/tgi/ui/personaldata/PersonalDataViewModel.kt
@@ -1,13 +1,21 @@
 package com.faculdade.tgi.ui.personaldata
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.faculdade.tgi.model.PersonalDataModel
+import com.faculdade.tgi.repository.personaldata.PersonalDataRepository
 
-class PersonalDataViewModel : ViewModel() {
+class PersonalDataViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is personal data Fragment"
+    private val repository = PersonalDataRepository(application.applicationContext)
+
+    private val _loadData = MutableLiveData<PersonalDataModel>()
+    val loadData: LiveData<PersonalDataModel> = _loadData
+
+    fun loadData() {
+        _loadData.value = repository.get()
     }
-    val text: LiveData<String> = _text
 }
